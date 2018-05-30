7c32663d6d9246a8434e2af29a6f233c8e3e449f
RiverMagnetic <magneticcontinuum@gmail.com>
2018-05-29 20:20:08 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
 console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[1];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-if (cardsInPlay.length === 2) { 
-if (cardsInPlay[0] === cardsInPlay[2]) {
- alert('You found a match!');
- } else {
- alert('Sorry, try again.');
- }
- }
-console.log("User flipped " + cardThree);
+var checkForMatch = function(){
+if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+}
+}
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2) { 
+	checkForMatch()
+	 }
+}
+flipCard(0);
+flipCard(2);
